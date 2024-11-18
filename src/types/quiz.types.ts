1bf106977fa03f6334539216476dc752df67ae33
Precious Akpan <49040483+precious-akpan@users.noreply.github.com>
2024-11-18 17:30:49 +0100

    types setup

---
+++ src/types/quiz.types.ts
@@ -0,0 +1,12 @@
+export interface Question {
+    category: string;
+    type: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[];
+}
+
+export interface QuizQuestion extends Question {
+    answers: string[];
+}
