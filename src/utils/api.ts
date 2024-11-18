6fed09d335f07e637385aebf84c842e546b251bb
Precious Akpan <49040483+precious-akpan@users.noreply.github.com>
2024-11-18 17:56:20 +0100

    added api for questions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,8 @@ import { Question, QuizQuestion } from '../types/quiz.types';
 
 export const fetchQuestions = async (): Promise<QuizQuestion[]> => {
     try {
-        const { data } = await axios.get('https://api.example.com/questions');
+        // const { data } = await axios.get('https://api.example.com/questions');
+        const { data } = await axios.get('https://opentdb.com/api.php?amount=10');
         return data.results.map((question: Question) => ({
             ...question,
             answers: [question.correct_answer, ...question.incorrect_answers].sort(() => Math.random() - 0.5),
