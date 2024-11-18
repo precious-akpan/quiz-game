6e49d1ea43c8bd80cda1174ef94e767a8da20156
Precious Akpan <49040483+precious-akpan@users.noreply.github.com>
2024-11-18 18:14:51 +0100

    added styles to Question.tsx

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,7 @@
+// src/components/Question.tsx
 import React from 'react';
 import { QuizQuestion } from '../types/quiz.types';
+import styles from './Question.module.css'; // Import the CSS module
 
 interface QuestionProps {
     question: QuizQuestion;
@@ -8,11 +10,11 @@ interface QuestionProps {
 
 const Question: React.FC<QuestionProps> = ({ question, onAnswer }) => {
     return (
-        <div>
-            <h2>{question.question}</h2>
-            <ul>
+        <div className={styles.questionBox}>
+            <h2 className={styles.questionText}>{question.question}</h2>
+            <ul className={styles.answerList}>
                 {question.answers.map((answer, index) => (
-                    <li key={index} onClick={() => onAnswer(answer)}>
+                    <li key={index} className={styles.answerItem} onClick={() => onAnswer(answer)}>
                         {answer}
                     </li>
                 ))}
